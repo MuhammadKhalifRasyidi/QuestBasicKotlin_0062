39c312c5c1adff194f2e803166511bbd39e35170
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-10-13 18:20:35 +0700

    membuat function ContohList yang menampilkan list

---
+++ app/src/main/java/com/example/questbasickotlin/Collection.kt
@@ -0,0 +1,11 @@
+package com.example.questbasickotlin
+
+// List adalah kumpulan data yang disusun secara berurutan dari
+// List bersifat prdered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List  Read-Only menggunakan listOf
+// List mutable menggunakan metableListOf
+
+fun ContohList() {
+    println("=== List ===")
+}
