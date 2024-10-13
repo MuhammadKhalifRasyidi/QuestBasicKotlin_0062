0fd09f57177e4758222d27eb7fb20b02e941ca69
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-10-13 21:18:07 +0700

    membuat function withParameter dan print untuk menampilkan

--- app/src/main/java/com/example/questbasickotlin/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin/Functions.kt
@@ -1,6 +1,12 @@
 package com.example.questbasickotlin
 
 fun withoutParameter() {
-    println("=== Without Parameter ===")
+    println("== without Parameter ==")
     println("Hellow, World")
+}
+
+fun withParameter(name:String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
 }
