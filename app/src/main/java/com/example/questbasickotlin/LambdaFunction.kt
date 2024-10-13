8ce7f7a32a18c8237f5f237bf31d9d4acbcfa5d4
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-10-13 21:29:19 +0700

    membuat function uppercaseString dan return untuk mengembalikan nilai

---
+++ app/src/main/java/com/example/questbasickotlin/LambdaFunction.kt
@@ -0,0 +1,10 @@
+package com.example.questbasickotlin
+
+// Lambda Functions
+// Lambda Functions adalah fungsi yang tidak memiliki nama
+// Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+// lambda Functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
