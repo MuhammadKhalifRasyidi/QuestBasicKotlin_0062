50a159c9c0500141ddd2194957281dec3261269c
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-10-13 21:22:16 +0700

    membuat function withDefaultParameter dan print untuk menampilkan

--- app/src/main/java/com/example/questbasickotlin/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin/Functions.kt
@@ -15,7 +15,14 @@ fun withParameter(name:String) {
 //named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
 fun withNamedArgument(name: String, age: Int) {
     println()
-    println("== with Named Argument ==")
+    println("== withNamedArgument ==")
     println("Hello, $name! You are $age years old.")
 }
 
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! you are $age years old.")
+}
