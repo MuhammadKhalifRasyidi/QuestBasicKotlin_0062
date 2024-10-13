ef2f61fd14d7b91c6e8db14885e895fbe8358228
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-10-13 20:37:49 +0700

    membuat function ConditionalStatement dan print untuk menampilkan

---
+++ app/src/main/java/com/example/questbasickotlin/ConditionalExpression.kt
@@ -0,0 +1,9 @@
+package com.example.questbasickotlin
+
+//Conditional expression
+//Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+//Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+}
