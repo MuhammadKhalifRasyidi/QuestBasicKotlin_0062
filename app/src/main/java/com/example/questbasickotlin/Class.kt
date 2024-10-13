74fc8c2d371c454e95418257031035db82566e44
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-10-13 18:01:57 +0700

    membuat function main dengan isi contact ID dan email serta menampilkan nilai email.

--- app/src/main/java/com/example/questbasickotlin/Class.kt
+++ app/src/main/java/com/example/questbasickotlin/Class.kt
@@ -3,3 +3,12 @@ package com.example.questbasickotlin
 class Motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+    //prints the value of the property: email
+    println(contact.email)
+
+
+}
