088ddf5bf6dddbdc456cc62bcbec6997ba5d6154
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-10-13 21:31:20 +0700

    membuat function main untuk memanggil uppercaseString dan print untuk menampilkan

--- app/src/main/java/com/example/questbasickotlin/LambdaFunction.kt
+++ app/src/main/java/com/example/questbasickotlin/LambdaFunction.kt
@@ -8,3 +8,10 @@ package com.example.questbasickotlin
 fun uppercaseString(string: String): String {
     return string.uppercase()
 }
+
+//Dapat ditulis dalam ekspressi lambda sbb :
+fun main() {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+    //HELLO
+}
